2f67db08036a2ab51e5ec266a8e6d0adc2d48b3f
Emilie G <adamanska@gmail.com>
2017-08-02 15:21:30 +0200

    Ready for combat

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 const transactionRoutes = require('./api/transactionRoutes');
 
 var router = express.Router();
-//fbehkru
+
 router.use('/api', transactionRoutes);
 
 module.exports = router;
